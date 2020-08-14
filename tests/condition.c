2f77b05850dd31ccd7b55c2901f43fb9a30e59e0
Lionel Blatter <lionel.blatter@kit.edu>
2020-08-14 16:49:34 +0200

    Update Examples/Tests

--- tests/condition.c
+++ tests/condition.c
@@ -20,35 +20,81 @@
 
 
 /*@ axiomatic test2{
+
   @ predicate c1(integer r) = 0 <= r < 2;
-  @ predicate c2(integer r) = 5 <= r < 8;
+  @ predicate c2(integer r) = 5 <= r < 7;
 
   @ predicate c3(integer r) = 2 <= r < 5;
-  @ predicate c4(integer r) = 8 <= r < 10;
+  @ predicate c4(integer r) = 7 <= r < 10;
+
+  @ predicate c12(integer r1, integer r2) = c1(r1) && c2(r2);
+  @ predicate c34(integer r1, integer r2) = c3(r1) && c4(r2);
+  @ predicate default(integer r1, integer r2) = !c12(r1,r2) && !c34(r1,r2);
 }*/
 
 
+// pas d'appelle de procedure dans un premier temps (peut marcher si les communication sont toujour interne a la fonction, send est recve subit la meme suraproximation)
+// pas de nouveau communicateur, WP le support pas l'allocation dynamique
 
+
+// Use the new specification language of RPP to write those properties
+
+/*@ requires 0 <= r1 < 5 && 5 <= r2 < 10;*/
 void f(int r1, int r2){
   int d = r1 + 5;
   int s = r2 - 5;
 
-  //relationnelle: use the new notation
+  /*@ assert satisfies_relation_behavior_1:
+    @   c12(r1, r2) ==>
+    @   test(r1,d) && test(s,r2);*/
+  /*@ assert satisfies_relation_behavior_2:
+    @   c34(r1, r2) ==>
+    @   test(r1,d) && test(s,r2);*/
 
-  /*@ assert test(s,d) ==> (c2(d) <==> c1(s));*/  //????????????
+  /*@ assert not_equal: r1 != r2;*/
 
+  /*@ assert disjointe_behavior:
+    @   !(c12(r1, r2) && c34(r1,r2));*/
+  /*@ assert complete_behavior:
+    @   c12(r1, r2) || c34(r1,r2) || default(r1,r2);*/
 
-  /*@ assert c1(s) && c2(d) ==> s != d;*/ // ?????????
+  /*@ assert dijonction_1: !c1(r1) && c2(r2) ==> !test(r1,r2);*/
+  /*@ assert dijonction_2: c1(r1) && !c2(r2) ==> !test(r1,r2);*/
 
-
+  /*@ assert dijonction_3: !c3(r1) && c4(r2) ==> !test(r1,r2);*/
+  /*@ assert dijonction_4: c3(r1) && !c4(r2) ==> !test(r1,r2);*/
 
   return;
 }
 
+/*@ requires 0 <= rank1 && 0 <= rank2 ;
+  @ requires test(rank1,rank2);*/
+void color (int rank1, int rank2){
+  /*@ ghost int color1 = 0;*/
+
+  if (rank1 < 5) {
+    /*@ ghost color1 ++;*/
+  } else{
+    if (rank1 < 10){
+    }
+  }
+
+  //----------------------------------
+  /*@ ghost int color2 = 0;*/
+
+  if (rank2 < 5) {
+  } else{
+    if (rank2 < 10){
+      /*@ ghost color2 ++;*/
+    }
+  }
+
+  /*@ assert color1 == color2;*/
+}
 
 
-int main(int argc, char **argv)
-{
+
+int main(int argc, char **argv){
   int data = 0;
   int my_rank = 0, num_procs = 0;
   /* Initialize the infrastructure necessary for communication */
@@ -66,31 +112,11 @@ int main(int argc, char **argv)
 
     data = my_rank;
     /* Send messages to other processes with my_rank + 5*/
-    /*@ assert satisfies_relation_behavior_1:
-      @   c1(__MPI_COMM_WORLD_rank_ACSL) ==>
-      @   test(__MPI_COMM_WORLD_rank_ACSL,my_rank + 5);*/
-    /*@ assert satisfies_relation_behavior_2:
-      @   c3(__MPI_COMM_WORLD_rank_ACSL) ==>
-      @   test(__MPI_COMM_WORLD_rank_ACSL,my_rank + 5);*/
-    /*@ assert complete_behavior:
-      @   c1(__MPI_COMM_WORLD_rank_ACSL) || c3(__MPI_COMM_WORLD_rank_ACSL);*/
-    /*@ assert disjointe_behavior:
-      @   !(c1(__MPI_COMM_WORLD_rank_ACSL) && c3(__MPI_COMM_WORLD_rank_ACSL));*/
     MPI_Ssend(&data, 1, MPI_INT, my_rank + 5, 0, MPI_COMM_WORLD);
   } else{
     if (my_rank < 10){
 
       /* Receive message from process my_rank-5 */
-      /*@ assert satisfies_relation_behavior_1:
-	@   c2(__MPI_COMM_WORLD_rank_ACSL) ==>
-	@   test(my_rank-5,__MPI_COMM_WORLD_rank_ACSL);*/
-      /*@ assert satisfies_relation_behavior_2:
-	@   c4(__MPI_COMM_WORLD_rank_ACSL) ==>
-	@   test(my_rank-5,__MPI_COMM_WORLD_rank_ACSL);*/
-      /*@ assert complete_behavior:
-	@   c2(__MPI_COMM_WORLD_rank_ACSL) || c4(__MPI_COMM_WORLD_rank_ACSL);*/
-      /*@ assert disjointe_behavior:
-	@   !(c2(__MPI_COMM_WORLD_rank_ACSL) && c4(__MPI_COMM_WORLD_rank_ACSL));*/
       MPI_Recv(&data, 1, MPI_INT, my_rank-5, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
   }
