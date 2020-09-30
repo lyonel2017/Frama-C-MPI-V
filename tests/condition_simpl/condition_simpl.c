b5f8175c7591c3c6cdb7d8f4e707577e135e62ef
Lionel Blatter <lionel.blatter@kit.edu>
2020-09-30 16:07:04 +0200

    Add example condition_simpl.

---
+++ tests/condition_simpl/condition_simpl.c
@@ -0,0 +1,44 @@
+#include "mpi.h"
+
+//frama-c-gui -mpi-v -wp-driver ../../share/mpi.driver,the_protocol.driver,size.driver condition_simpl.c
+
+int main(int argc, char **argv){
+  int data = 0;
+  int my_rank = 0, num_procs = 0;
+
+  /* Initialize the infrastructure necessary for communication */
+  MPI_Init(&argc, &argv);
+
+  /* Identify this process */
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+  /* Find out how many total processes are active */
+  MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+  /* Until this point, all programs have been doing exactly the same.
+     Here, we check the rank to distinguish the roles of the programs */
+  if (my_rank == 0) {
+
+    data = my_rank;
+    /* Send messages to processes 1*/
+    MPI_Ssend(&data, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
+  } else{
+    if (my_rank == 1){
+
+      /* Receive message from process 0 */
+      MPI_Recv(&data, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    /* ghost code*/
+
+    else {
+      toskip();
+    }
+
+    /*ghost code*/
+  }
+
+  /* Tear down the communication infrastructure */
+  MPI_Finalize();
+  return 0;
+}
