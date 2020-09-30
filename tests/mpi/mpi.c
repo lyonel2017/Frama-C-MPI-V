d511e0c9955d67ed629bb5cd4dbe24cb3b33fb0f
Lionel Blatter <lionel.blatter@kit.edu>
2020-09-30 16:05:56 +0200

    Update example mpi.

--- tests/mpi/mpi.c
+++ tests/mpi/mpi.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "mpi.h"
 
+frama-c-gui -mpi-v -wp-driver ../../share/mpi.driver,the_protocol.driver,size.driver mpi.c
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
