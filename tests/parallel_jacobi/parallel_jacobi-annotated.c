f409230619801bac1cbbbdba29247b81bb9a7720
Tobias Hombücher <tobias.hombuecher@student.kit.edu>
2021-01-17 15:31:31 +0100

    Removes unnecessary parallel_jacobi files

--- tests/parallel_jacobi/parallel_jacobi-annotated.c
+++ tests/parallel_jacobi/parallel_jacobi-annotated.c
@@ -15,7 +15,7 @@
 
 
 #define MAX_DIM  10000
-#define NUM_ITER 1000000 
+#define NUM_ITER 1000000
 
 #define MATRIX_DECL(var) float var[MAX_DIM * MAX_DIM]
 
@@ -39,7 +39,7 @@ void Jacobi_iteration(float* A_local, float* x_local, float* b_local, float* x_o
 
 /**
  * HELPER:
- * for most applications this function can be left out 
+ * for most applications this function can be left out
  * if the receive buffers are initialized after MPI operations
  * */
 /*@ ensures \initialized(vector + (0..n-1));
@@ -50,18 +50,18 @@ void init(float* vector, int n);
 
 
 /**
- * requires timeout = 90, process = 32 
- * tested with wp-fix 
+ * requires timeout = 90, process = 32
+ * tested with wp-fix
  **/
 int main( int argc, char** argv){
-    // CHANGED: MAX_DIM s.t. MAX_DIM * MAX_DIM can be int 
+    // CHANGED: MAX_DIM s.t. MAX_DIM * MAX_DIM can be int
     int        p;
     int        rank;
     int        n;
     int        n_by_p;
     int        iter;
     int        converged;
-    // CHANGED: declaration without #define 
+    // CHANGED: declaration without #define
     float      A_initial[MAX_DIM * MAX_DIM];
     float      A_local[MAX_DIM * MAX_DIM];
 
@@ -84,16 +84,16 @@ int main( int argc, char** argv){
     if (rank == 0) {
       readMatrix(A_initial, n);
       readVector(b_initial, n);
-    } 
+    }
 
-    MPI_Scatter(A_initial, n_by_p * n, MPI_FLOAT, A_local, n_by_p * n , MPI_FLOAT, 0, MPI_COMM_WORLD); 
+    MPI_Scatter(A_initial, n_by_p * n, MPI_FLOAT, A_local, n_by_p * n , MPI_FLOAT, 0, MPI_COMM_WORLD);
     init(A_local, n_by_p*n);
-    
+
     MPI_Scatter(b_initial, n_by_p, MPI_FLOAT, x_local, n_by_p, MPI_FLOAT, 0, MPI_COMM_WORLD);
     init(x_local, n_by_p);
     //@ assert \initialized(x_local + (0..n_by_p-1));
     //@ assert \valid(x_local + (0..n_by_p-1));
-    
+
     init(b_local, n_by_p);
     //@ assert \initialized(b_local + (0..n_by_p-1));
     //@ assert \valid(b_local + (0..n_by_p-1));
@@ -101,16 +101,16 @@ int main( int argc, char** argv){
     init(x_temp1, n_by_p*p);
     //@ assert \initialized(x_temp1 + (0..n_by_p*p-1));
     //@ assert \valid(x_temp1 + (0..n_by_p*p-1));
-    
+
     x_new = x_temp1;
     //@ assert \initialized(x_new + (0..n_by_p*p-1));
     //@ assert \valid(x_new + (0..n_by_p*p-1));
-    x_old = x_temp1;  
+    x_old = x_temp1;
     //@ assert \initialized(x_old + (0..n_by_p*p-1));
     //@ assert \valid(x_old + (0..n_by_p*p-1));
 
-    // ADDED: necessary for loop invariant 
-    tmp = x_temp1; 
+    // ADDED: necessary for loop invariant
+    tmp = x_temp1;
     //@ assert \initialized(tmp + (0..n_by_p*p-1));
     //@ assert \valid(tmp + (0..n_by_p*p-1));
 
@@ -128,7 +128,7 @@ int main( int argc, char** argv){
       @ loop invariant \valid(tmp + (0..n_by_p*p-1));
       @ loop invariant \valid(x_local + (0..n_by_p-1));
       @ loop assigns protocol, iter,tmp, x_old, x_new, x_local[0..n_by_p-1];
-      @ loop variant NUM_ITER - iter; 
+      @ loop variant NUM_ITER - iter;
       @*/
     // CHANGED: starts at 0 instead of 1, end changed respectively
     //          necessary s.t. while can be used in protocol
@@ -144,7 +144,7 @@ int main( int argc, char** argv){
       //@ assert \initialized(x_local + (0..n_by_p-1));
       //@ assert \valid(x_local + (0..n_by_p-1));
 
-      //@ ghost unroll(); 
+      //@ ghost unroll();
       //@ ghost assoc();
       MPI_Allgather(x_local, n_by_p, MPI_FLOAT, x_new, n_by_p, MPI_FLOAT, MPI_COMM_WORLD);
       init(x_new, n_by_p);
@@ -160,10 +160,10 @@ int main( int argc, char** argv){
       x_new = tmp;
       //@ assert \initialized(x_new + (0..n_by_p*p-1));
       //@ assert \valid(x_new + (0..n_by_p*p-1));
-    } 
+    }
     //@ ghost toskip();
     MPI_Gather(x_new, n_by_p, MPI_FLOAT, x_final, n_by_p, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    
+
     // REMOVED: no I/O
     // if (rank == 0) {
     //   printVector(x_new, n);
@@ -171,4 +171,4 @@ int main( int argc, char** argv){
 
     MPI_Finalize();
     //@ assert \false;
-} 
+}
