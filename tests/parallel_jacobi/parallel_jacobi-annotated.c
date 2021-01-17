dcbec16ae4c93bd295897004f9eac51b8ccebaab
Tobias Hombücher <tobias.hombuecher@student.kit.edu>
2021-01-17 16:16:06 +0100

    Removes unnecessary asserts; Allows 1<=size<=10; Removes trailing whitespaces

--- tests/parallel_jacobi/parallel_jacobi-annotated.c
+++ tests/parallel_jacobi/parallel_jacobi-annotated.c
@@ -36,21 +36,8 @@ void printVector(float* vector, int dim);
   @*/
 void Jacobi_iteration(float* A_local, float* x_local, float* b_local, float* x_old, int n, int p);
 
-
 /**
- * HELPER:
- * for most applications this function can be left out
- * if the receive buffers are initialized after MPI operations
- * */
-/*@ ensures \initialized(vector + (0..n-1));
-  @ ensures \valid(vector + (0..n-1));
-  @ assigns \nothing;
-  @*/
-void init(float* vector, int n);
-
-
-/**
- * requires timeout = 90, process = 32
+ * requires timeout = 15
  * tested with wp-fix
  **/
 int main( int argc, char** argv){
@@ -87,42 +74,20 @@ int main( int argc, char** argv){
     }
 
     MPI_Scatter(A_initial, n_by_p * n, MPI_FLOAT, A_local, n_by_p * n , MPI_FLOAT, 0, MPI_COMM_WORLD);
-    init(A_local, n_by_p*n);
-
     MPI_Scatter(b_initial, n_by_p, MPI_FLOAT, x_local, n_by_p, MPI_FLOAT, 0, MPI_COMM_WORLD);
-    init(x_local, n_by_p);
-    //@ assert \initialized(x_local + (0..n_by_p-1));
-    //@ assert \valid(x_local + (0..n_by_p-1));
-
-    init(b_local, n_by_p);
-    //@ assert \initialized(b_local + (0..n_by_p-1));
-    //@ assert \valid(b_local + (0..n_by_p-1));
     MPI_Allgather(b_local, n_by_p, MPI_FLOAT, x_temp1, n_by_p, MPI_FLOAT, MPI_COMM_WORLD);
-    init(x_temp1, n_by_p*p);
-    //@ assert \initialized(x_temp1 + (0..n_by_p*p-1));
-    //@ assert \valid(x_temp1 + (0..n_by_p*p-1));
 
     x_new = x_temp1;
-    //@ assert \initialized(x_new + (0..n_by_p*p-1));
-    //@ assert \valid(x_new + (0..n_by_p*p-1));
     x_old = x_temp1;
-    //@ assert \initialized(x_old + (0..n_by_p*p-1));
-    //@ assert \valid(x_old + (0..n_by_p*p-1));
 
     // ADDED: necessary for loop invariant
     tmp = x_temp1;
-    //@ assert \initialized(tmp + (0..n_by_p*p-1));
-    //@ assert \valid(tmp + (0..n_by_p*p-1));
 
     /*@ loop invariant 0 <= iter <= NUM_ITER;
       @ loop invariant getFirst(get_type(protocol)) ==
       @  getNext(split (getFirst(\at(get_type(protocol),LoopEntry)),iter));
       @ loop invariant getNext(get_type(protocol)) ==
-      @    getNext(\at(get_type(protocol),LoopEntry));
-      @ loop invariant \initialized(x_old + (0..n_by_p*p-1));
-      @ loop invariant \initialized(x_new + (0..n_by_p*p-1));
-      @ loop invariant \initialized(tmp + (0..n_by_p*p-1));
-      @ loop invariant \initialized(x_local + (0..n_by_p-1));
+      @  getNext(\at(get_type(protocol),LoopEntry));
       @ loop invariant \valid(x_old + (0..n_by_p*p-1));
       @ loop invariant \valid(x_new + (0..n_by_p*p-1));
       @ loop invariant \valid(tmp + (0..n_by_p*p-1));
@@ -133,33 +98,16 @@ int main( int argc, char** argv){
     // CHANGED: starts at 0 instead of 1, end changed respectively
     //          necessary s.t. while can be used in protocol
     for (iter = 0; iter < NUM_ITER; iter++){
-      //@ assert \initialized(x_old + (0..n_by_p*p-1));
-      //@ assert \initialized(x_new + (0..n_by_p*p-1));
-      //@ assert \initialized(tmp + (0..n_by_p*p-1));
-      //@ assert \valid(x_old + (0..n_by_p*p-1));
-      //@ assert \valid(x_new + (0..n_by_p*p-1));
-      //@ assert \valid(tmp + (0..n_by_p*p-1));
 
       Jacobi_iteration(A_local, x_local, b_local, x_old, n, p);
-      //@ assert \initialized(x_local + (0..n_by_p-1));
-      //@ assert \valid(x_local + (0..n_by_p-1));
 
       //@ ghost unroll();
       //@ ghost assoc();
       MPI_Allgather(x_local, n_by_p, MPI_FLOAT, x_new, n_by_p, MPI_FLOAT, MPI_COMM_WORLD);
-      init(x_new, n_by_p);
-      //@ assert \initialized(x_new + (0..n_by_p*p-1));
-      //@ assert \valid(x_new + (0..n_by_p*p-1));
 
       tmp = x_old;
-      //@ assert \initialized(tmp + (0..n_by_p*p-1));
-      //@ assert \valid(tmp + (0..n_by_p*p-1));
       x_old = x_new;
-      //@ assert \initialized(x_old + (0..n_by_p*p-1));
-      //@ assert \valid(x_old + (0..n_by_p*p-1));
       x_new = tmp;
-      //@ assert \initialized(x_new + (0..n_by_p*p-1));
-      //@ assert \valid(x_new + (0..n_by_p*p-1));
     }
     //@ ghost toskip();
     MPI_Gather(x_new, n_by_p, MPI_FLOAT, x_final, n_by_p, MPI_FLOAT, 0, MPI_COMM_WORLD);
