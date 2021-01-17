835ccb9f27e12c2c3bf58feda97e79919487d9b7
Tobias Hombücher <tobias.hombuecher@student.kit.edu>
2021-01-17 10:16:12 +0100

    Fixes error in reduce_float example

--- tests/reduce_float/reduce_float.c
+++ tests/reduce_float/reduce_float.c
@@ -9,14 +9,15 @@ int main(argc,argv) int argc; char *argv[];
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    float local = 1.0f; 
-    float global = 0.0f; 
+    float local = 1.0f;
+    float global = 0.0f;
 
 
     MPI_Reduce(&local, &global, 1, MPI_FLOAT, MPI_SUM, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        // printf("sum: %f\n", global); 
-    } 
+        // printf("sum: %f\n", global);
+    }
     MPI_Finalize();
+    //@ assert \false;
 }
