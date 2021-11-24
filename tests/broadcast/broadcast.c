a271666ccf672c16936379efa8878c676a3129fd
Lionel Blatter <lionel.blatter@kit.edu>
2021-11-24 13:51:41 +0100

    Fix broadcast example

--- tests/broadcast/broadcast.c
+++ tests/broadcast/broadcast.c
@@ -22,10 +22,12 @@ int main(int argc, char **argv){
     data = my_rank;
     /* Send messages to processes 1*/
     MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //@ ghost toskip();
   } else{
 
     /* Receive message from process 0 */
     MPI_Bcast(&data, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    //@ ghost toskip();
   }
 
   /* Tear down the communication infrastructure */
