739b3b4c4254b6e6f40419d8bc1cac2dd9ebb744
Lionel Blatter <lionel.blatter@kit.edu>
2022-01-07 15:35:05 +0100

    Clean up in example

--- tests/condition_continu/condition_continu.c
+++ tests/condition_continu/condition_continu.c
@@ -1,10 +1,10 @@
 #include "mpi.h"
 
-//frama-c-gui -mpi-v -wp-driver ../../share/mpi.driver,the_protocol.driver,size.driver condition_continu.c
+//frama-c-gui -mpi-v -wp-weak-int-model -wp-driver ../../share/mpi.driver,the_protocol.driver,size.driver condition_continu.c
 
 int main(int argc, char **argv){
-  int data = -1;
-  int my_rank = 0, num_procs = 0;
+  int data = -1, my_rank = 0, num_procs = 0;
+  /*@ ghost int g = data;*/
 
   /* Initialize the infrastructure necessary for communication */
   MPI_Init(&argc, &argv);
@@ -22,18 +22,17 @@ int main(int argc, char **argv){
     data = my_rank;
   l1:;
     /*@ ghost
-      @ int g = data;
+      @ g = data;
       @ MPI_GIntBcast(&g, 1, 0);*/
     /*@ ghost assoc();*/
     /* Send messages to processes 1*/
     MPI_Ssend(&data, 1, MPI_INT, 1, 1, MPI_COMM_WORLD);
     MPI_Recv(&data, 1, MPI_INT, 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    /*@ assert data == \at(data,l1) + 1;*/
+    /*@ assert data == my_rank + 1;*/
   } else{
     if (my_rank == 1){
     l2:;
       /*@ ghost
-        @ int g = my_rank;
         @ MPI_GIntBcast(&g, 1, 0);*/
       /*@ ghost assoc();*/
       /* Receive message from process 0 */
@@ -43,7 +42,6 @@ int main(int argc, char **argv){
     }
     /*@ ghost
        else {
-          int g = my_rank;
           MPI_GIntBcast(&g, 1, 0);
           assoc();
           toskip();
