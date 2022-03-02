04ec50cd6227f154690ded40956566d6e4767170
Lionel Blatter <lionel.blatter@kit.edu>
2022-03-02 11:24:19 +0100

    Clean the example

--- tests/diffusion1D/diffusion1D.c
+++ tests/diffusion1D/diffusion1D.c
@@ -1,15 +1,15 @@
-/** 
+/**
  * ParTypes - http://gloss.di.fc.ul.pt/ParTypes
  *
  * The 1-D heat diffusion program.
  *
  * Adapted from:
- *   FEVS: A Functional Equivalence Verification Suite. 
+ *   FEVS: A Functional Equivalence Verification Suite.
  *   http://vsl.cis.udel.edu/fevs/index.html
- *   
+ *
  * Version: $Id: diffusion1D.prot 4690 2015-06-24 10:18:56Z edrdo $
  */
- 
+
 // MPI header
 #include <mpi.h>
 
@@ -17,25 +17,25 @@
 #define MAXLEN 10000
 
 // External functions
-int getNumIterations(void); 
+int getNumIterations(void);
 int getProblemSize(int procs);
 int getWstep();
 void getInitialData(int rank, float* data, int len);
 int compute(float* u, float* u_new, int len, int wstep);
 
 int main(int argc, char** argv) {
-  int procs;  // process count 
+  int procs;  // process count
   int rank; // process rank */
-  int n ; // number of discrete points including endpoints 
-  int local_n; // size of local data = n / procs 
-  int numIter ; // number of time steps 
-  int wstep ;  // write frame every this many time steps 
+  int n ; // number of discrete points including endpoints
+  int local_n; // size of local data = n / procs
+  int numIter ; // number of time steps
+  int wstep ;  // write frame every this many time steps
   int i, iter, start, stop, left, right;
   MPI_Status status;
-  float u[MAXLEN+2]; // temperature function 
-  float u_new[MAXLEN+2];  // temp. used to update u 
-  float buf[MAXLEN+1]; // Local buffer used on rank 0 
-  
+  float u[MAXLEN+2]; // temperature function
+  float u_new[MAXLEN+2];  // temp. used to update u
+  float buf[MAXLEN+1]; // Local buffer used on rank 0
+
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &procs);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -55,7 +55,7 @@ int main(int argc, char** argv) {
       getInitialData(i, buf, local_n);
       MPI_Send(buf, local_n, MPI_FLOAT, i, 0, MPI_COMM_WORLD);
     }
-  } else { 
+  } else {
     MPI_Recv(&u[1], local_n, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, &status);
   }
   start = rank == 0 ? 2 : 1;
@@ -65,8 +65,8 @@ int main(int argc, char** argv) {
   for (iter = 0; iter < numIter; iter++) {
     if (rank == 0) {
       MPI_Recv(&u[local_n+1], 1, MPI_FLOAT, right, 0, MPI_COMM_WORLD, &status);
-    } 
-    else if (rank == procs-1) { 
+    }
+    else if (rank == procs-1) {
       MPI_Send(&u[1], 1, MPI_FLOAT, left, 0, MPI_COMM_WORLD);
     }
     else {
@@ -78,11 +78,10 @@ int main(int argc, char** argv) {
     } else if (rank == procs - 1) {
       MPI_Recv(&u[0], 1, MPI_FLOAT, left, 0, MPI_COMM_WORLD, &status);
     } else {
-      MPI_Recv(&u[0], 1, MPI_FLOAT, left, 0, MPI_COMM_WORLD, &status);   
+      MPI_Recv(&u[0], 1, MPI_FLOAT, left, 0, MPI_COMM_WORLD, &status);
     }
     compute(u, u_new, local_n, wstep);
   }
   MPI_Finalize();
   return 0;
 }
-
